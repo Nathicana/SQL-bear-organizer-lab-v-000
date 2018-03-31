67a5abbe67fef19c6c36577e9eb97568f13420cd
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-03-31 21:06:53 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -11,6 +11,6 @@ VALUES ("Melissa", 5, "F", "lavender", "sweet", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Grinch", 20, "M", "green", "nasty", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Wendy", 2, "F", "pink", "silly", 1);
+VALUES ("Wendy", 2, "F", "pink", "goofy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES (NULL, 2, "M", "black", "aggressive", 0);
